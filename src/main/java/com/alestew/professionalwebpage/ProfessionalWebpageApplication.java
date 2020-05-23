ffcf0bdf583e47049fe5d1231cdc3efd9553c140
alex-stewart <imalexstewart+git@gmail.com>
2020-05-23 19:19:51 +0100

    spring boot initialiser

---
+++ src/main/java/com/alestew/professionalwebpage/ProfessionalWebpageApplication.java
@@ -0,0 +1,13 @@
+package com.alestew.professionalwebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalWebpageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfessionalWebpageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alestew/professionalwebpage/ProfessionalWebpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alestew.professionalwebpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionalWebpageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
